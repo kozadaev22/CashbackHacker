c9adc9fcae0df306707088fe84ead51c31fdd55e
kozadaev22 <kozadaev22@mail.ru>
2022-08-07 20:22:16 +0300

    Add branch and tests for JUnit4

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -1,3 +1,26 @@
 package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CashbackHackerTest {
+    CashbackHackerService service = new CashbackHackerService();
+    @Test
+    public void AmountMoreBoundary() {
+        int actual = service.remain(2500);
+        int expected = 500;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void AmountLessBoundary() {
+        int actual = service.remain(640);
+        int expected = 360;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void AmountEqualsBoundary() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
 }
